be0dcfed820a9662f39065cc49ed2e5993174d2f
Quek Kang Sheng <ks.quek.2023@smu.edu.sg>
2025-10-29 03:09:32 +0000

    Done ex1, 2 and 3

--- src/components/Ex1.vue
+++ src/components/Ex1.vue
@@ -1,21 +1,25 @@
 <script>
     //import component
-    
+    import SoundIcon from './subcomponents/SoundIcon.vue'
     export default { 
         data() {
             return {
                 soundLevel: 0
             }
+        },
+        components: {
+            SoundIcon
         }
     }
 </script>
 
 <template>
 
-   Sound level
+    Sound level
     <input type="number" min="0" max="3" v-model="soundLevel"> 
     <!-- TODO: Add Code Here to make use of the component -->
-    
-
+    <!-- If static variable you can just use soundlevel= x but since v-model is a dynamic variable, we need to use ":"
+    in front of the soundlevel  -->
+    <sound-icon :soundlevel="soundLevel"></sound-icon>
 </template>
 
--- src/components/subcomponents/SoundIcon.vue
+++ src/components/subcomponents/SoundIcon.vue
@@ -6,10 +6,13 @@
             }
         },
         // TODO: add code to define a necessary prop
+        props: {
+            soundlevel: Number
+        }
     }
 </script>
 
 <template>
     <!-- TODO: add your template code here. Use span element. --> 
-    
+    <span>{{ soundEmojis[soundlevel] }}</span>
 </template>
